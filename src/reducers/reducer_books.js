3e1f6ed7c7461108bf49ab3cf6b76edbccf453b8
ashikpanchangam <ashikpanchangam@gmail.com>
2018-02-10 13:20:35 -0800

    Created action creators and reducers and binded the action creators

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,6 +1,6 @@
 export default function() {
     return [
-      { title: 'JavScript The Good Parts' },
+      { title: 'JavaScript The Good Parts' },
       { title: 'Harry Potter' },
       { title: 'The Dark Tower' },
       { title: 'Eloquent Ruby' }
